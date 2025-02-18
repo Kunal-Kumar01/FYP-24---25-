9a89b538d86f16b321eeceb0d1f8489700c46f67
AmmarLokhandwala <ammarabbas652@gmail.com>
2025-02-18 12:51:46 +0500

    adjusted the role options on signup page

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -279,9 +279,9 @@ const SignUp = () => {
               value={credentials.roleId}
               required
             >
-              <option value="1">Role 1</option>
-              <option value="2">Role 2</option>
-              <option value="3">Role 3</option>
+              <option value="1">Admin</option>
+              <option value="2">Data Engineer</option>
+              <option value="3">Viewer</option>
             </select>
           </div>
 
